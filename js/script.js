15b3f81a1fc88116657f406751bcdf8bea39dd72
GUILHERME SANTOS SIQUEIRA <guilherme.ssiqueira4@senacsp.edu.br>
2023-11-22 21:57:46 -0300

    feat: updates

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new TypeIt (".animated", {
+      speed: 100,
+     
+      loop: true
+  }).type ('Estratégias de Marketing Digital', {delay: 500}).delete (32).type ('SEO- Search Engine Optimization', {delay:500}).delete (31).type ('Alojamento Cloud', {delay: 500}).delete (16).type ('Cibersegurança', {delay:500})
+   
+  .go ()
+})
+
+
 const btnMobile = document.getElementById('btn-responsivo'),
-      navMenu = document.querySelectorAll('.nav-menu');
+  navMenu = document.querySelectorAll('.nav-menu');
 
 function toggleMenu() {
-    const nav = document.getElementById('menu');
-    nav.classList.toggle('active');
+  const nav = document.getElementById('menu');
+  nav.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
@@ -16,4 +27,7 @@ const myCarouselElement = document.querySelector('#myCarousel')
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
-})
+});
+
+
+ 
